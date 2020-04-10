3cc07672ebbb9e98c7e4660bdafb821ce0b87359
codva <jeevesh@netsecure.in>
2020-04-10 20:24:09 +0530

    new one

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var taskList = [];
+
+
+//function addItem
+
+	
+
+function addItem(){
+  var task =document.getElementById("todoInput").value;
+  var ul = document.getElementById("myUl");
+  //alert(task);
+
+
+//triming sting
+	if (task.trim().length === 0 ){
+
+		return false;
+	} //end of if
+
+	else {
+	//make addItem for 5 items
+  if (taskList.length < 5){
+
+  	taskList.push(task);
+
+  	var li = document.createElement("li");
+  	var text = document.createTextNode(task);
+  	li.appendChild(text);
+
+  	var checkMark = document.createElement("button");
+  	checkMark.innerHTML = "&check;";
+  	checkMark.className = "listItemButton";
+ 	checkMark.setAttribute("onclick","checkMe(this)");
+	li.appendChild(checkMark);	//append check mark to list
+
+  	var crossMark = document.createElement("button");
+  	crossMark.innerHTML = "&#x2715";
+  	crossMark.className = "listItemButton";  
+
+  	crossMark.setAttribute("onclick", "removeMe(this)");
+  	li.appendChild(crossMark);  //append crossmark to list
+
+  	ul.appendChild(li);
+
+  	reset();
+  	
+
+  }// if end
+
+} //end of else
+} //end of function AddItem
